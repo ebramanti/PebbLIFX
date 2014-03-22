9f37120f70ef821d6f20e0db7524812085e5660b
Edward Bramanti <edward@bramanti.org>
2014-03-22 03:03:53 -0700

    Implemented mockup of pebble-app design & fixed some project files.

--- android-app/gen/com/jadengore/pebblifx/R.java
+++ android-app/gen/com/jadengore/pebblifx/R.java
@@ -29,8 +29,7 @@ public final class R {
     public static final class layout {
         public static final int activity_main=0x7f030000;
         public static final int activity_preferences=0x7f030001;
-        public static final int main=0x7f030002;
-        public static final int preferences=0x7f030003;
+        public static final int preferences=0x7f030002;
     }
     public static final class menu {
         public static final int main=0x7f070000;
--- pebble-app/src/mainMenu.c
+++ pebble-app/src/mainMenu.c
@@ -1,8 +1,8 @@
 #include "pebble.h"
 
 #define NUM_MENU_SECTIONS 2
-#define NUM_FIRST_MENU_ITEMS 3
-#define NUM_SECOND_MENU_ITEMS 1
+#define NUM_FIRST_MENU_ITEMS 1
+#define NUM_SECOND_MENU_ITEMS 3
 
 static Window *window;
 
@@ -18,22 +18,22 @@ static SimpleMenuItem first_menu_items[NUM_FIRST_MENU_ITEMS];
 static SimpleMenuItem second_menu_items[NUM_SECOND_MENU_ITEMS];
 
 // Menu items can optionally have icons
-static GBitmap *menu_icon_image;
+//static GBitmap *menu_icon_image;
 
-static bool special_flag = false;
+//static bool special_flag = false;
 
-static int hit_count = 0;
+//static int hit_count = 0;
 
 // You can capture when the user selects a menu icon with a menu item select callback
-static void menu_select_callback(int index, void *ctx) {
+/*static void menu_select_callback(int index, void *ctx) {
   // Here we just change the subtitle to a literal string
   first_menu_items[index].subtitle = "You've hit select here!";
   // Mark the layer to be updated
   layer_mark_dirty(simple_menu_layer_get_layer(simple_menu_layer));
-}
+} */
 
 // You can specify special callbacks to differentiate functionality of a menu item
-static void special_select_callback(int index, void *ctx) {
+/*static void special_select_callback(int index, void *ctx) {
   // Of course, you can do more complicated things in a menu item select callback
   // Here, we have a simple toggle
   special_flag = !special_flag;
@@ -52,7 +52,7 @@ static void special_select_callback(int index, void *ctx) {
 
   // Mark the layer to be updated
   layer_mark_dirty(simple_menu_layer_get_layer(simple_menu_layer));
-}
+} */
 
 // This initializes the menu upon window load
 static void window_load(Window *window) {
@@ -62,33 +62,30 @@ static void window_load(Window *window) {
   // Although we already defined NUM_FIRST_MENU_ITEMS, you can define
   // an int as such to easily change the order of menu items later
   int num_a_items = 0;
+  int num_b_items = 0;
 
   // This is an example of how you'd set a simple menu item
   first_menu_items[num_a_items++] = (SimpleMenuItem){
     // You should give each menu item a title and callback
-    .title = "First Item",
-    .callback = menu_select_callback,
+    .title = "All Lights"
+    //.callback = menu_select_callback,
   };
   // The menu items appear in the order saved in the menu items array
-  first_menu_items[num_a_items++] = (SimpleMenuItem){
-    .title = "Second Item",
+  second_menu_items[num_b_items++] = (SimpleMenuItem){
+    .title = "PebbLIFX 1"
     // You can also give menu items a subtitle
-    .subtitle = "Here's a subtitle",
-    .callback = menu_select_callback,
+    //.callback = menu_select_callback,
   };
-  first_menu_items[num_a_items++] = (SimpleMenuItem){
-    .title = "Third Item",
-    .subtitle = "This has an icon",
-    .callback = menu_select_callback,
-    // This is how you would give a menu item an icon
-    .icon = menu_icon_image,
+  second_menu_items[num_b_items++] = (SimpleMenuItem){
+    .title = "PebbLIFX 2"
+    //.callback = menu_select_callback,
   };
 
   // This initializes the second section
-  second_menu_items[0] = (SimpleMenuItem){
-    .title = "Special Item",
+  second_menu_items[num_b_items++] = (SimpleMenuItem){
+    .title = "PebbLIFX 3"
     // You can use different callbacks for your menu items
-    .callback = special_select_callback,
+    //.callback = special_select_callback,
   };
 
   // Bind the menu items to the corresponding menu sections
@@ -98,7 +95,7 @@ static void window_load(Window *window) {
   };
   menu_sections[1] = (SimpleMenuSection){
     // Menu sections can also have titles as well
-    .title = "Yet Another Section",
+    .title = "Bulbs",
     .num_items = NUM_SECOND_MENU_ITEMS,
     .items = second_menu_items,
   };
@@ -121,7 +118,7 @@ void window_unload(Window *window) {
   simple_menu_layer_destroy(simple_menu_layer);
 
   // Cleanup the menu icon
-  gbitmap_destroy(menu_icon_image);
+  //gbitmap_destroy(menu_icon_image);
 }
 
 int main(void) {
