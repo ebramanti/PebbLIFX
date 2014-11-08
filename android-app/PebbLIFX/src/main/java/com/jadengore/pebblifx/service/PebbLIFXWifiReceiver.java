4bc7939841f3907d476c24cc5a1983e6f6bfde41
Edward Bramanti <edward@bramanti.org>
2014-11-07 22:47:10 -0800

    Implemented check for phone on WiFi
    Allows for a service restart so the user does not have to do it by
    hand, and saves battery when not connected to a WiFi network

--- android-app/PebbLIFX/src/main/java/com/jadengore/pebblifx/service/PebbLIFXWifiReceiver.java
+++ android-app/PebbLIFX/src/main/java/com/jadengore/pebblifx/service/PebbLIFXWifiReceiver.java
@@ -3,27 +3,24 @@ package com.jadengore.pebblifx.service;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
-import android.net.wifi.WifiManager;
 import android.util.Log;
 
 public class PebbLIFXWifiReceiver extends BroadcastReceiver{
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        final String action = intent.getAction();
+        ConnectivityManager connManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo mWifi = connManager.getNetworkInfo(ConnectivityManager.TYPE_WIFI);
+        boolean connectedToWifi = mWifi.isConnected();
 
-        if(action.equals(WifiManager.NETWORK_STATE_CHANGED_ACTION)){
-            NetworkInfo info = intent.getParcelableExtra(WifiManager.EXTRA_NETWORK_INFO);
-            boolean connectedToWifi = info.isConnected();
-
-            if (connectedToWifi) {
-                Log.i("PebbLIFXWifiReceiver", "Wifi Connected, PebbLIFXService started.");
-                context.startService(new Intent(context, PebbLIFXService.class));
-            } else {
-                Log.i("PebbLIFXWifiReceiver", "Wifi DC'd, PebbLIFXService stopped.");
-                context.stopService(new Intent(context, PebbLIFXService.class));
-            }
+        if (connectedToWifi) {
+            Log.i("PebbLIFXWifiReceiver", "Wifi State Changed, PebbLIFXService refreshed.");
+            context.startService(new Intent(context, PebbLIFXService.class));
+        } else {
+            Log.i("PebbLIFXWifiReceiver", "Wifi DC'd, PebbLIFXService stopped.");
+            context.stopService(new Intent(context, PebbLIFXService.class));
         }
     }
 }
